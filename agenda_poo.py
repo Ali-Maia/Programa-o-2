f712c4b507c77cb66ae18ce4ee572d34c0931f40
Zorxrz <aliciaengcomp@gmail.com>
2021-07-14 10:08:56 -0300

    adicionando arquivo agenda

---
+++ agenda_poo.py
@@ -0,0 +1,32 @@
+#
+agenda=[]
+contatos={}
+#metodo é um verbo
+#atributo é um substantivo
+class Agenda():
+    def __init__(self,contatos,agenda):
+        self.contatos=contatos
+        self.agenda=agenda
+    #adicionar 
+    #excluir 
+    #listar
+    #modificar 
+
+
+
+class Menu():
+    def __init__(self) -> None:
+        pass
+    #funções presentes
+
+    
+class Contato():
+    def __init__(self,nome,telefone,email):
+        self.nome=nome
+        self.telefone=telefone
+        self.email=email
+        
+    #nome 
+    #email
+    #telefone
+    
