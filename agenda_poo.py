68f99a57d3d7813df73bac4d3c8abbc4b474ba19
Zorxrz <aliciaengcomp@gmail.com>
2021-07-15 10:32:49 -0300

    primeira defnião de classes

--- agenda_poo.py
+++ agenda_poo.py
@@ -7,17 +7,34 @@ class Agenda():
     def __init__(self,contatos,agenda):
         self.contatos=contatos
         self.agenda=agenda
+    #atributos
+    #contatos
+    #agenda 
+    def adicionar(self):
+        pass
+    def exluir(self):
+        pass
+    def listar(self):
+        pass
+    def modificar (self):
+        pass
+    
+    #metodos
     #adicionar 
     #excluir 
     #listar
     #modificar 
 
 
-
 class Menu():
-    def __init__(self) -> None:
+    def __init__(self,inicio,escolha):
+        self.inicio=inicio
+        self.escolha=escolha
         pass
-    #funções presentes
+    def funcoes(self):
+        pass
+    #metodos
+    #mostar as funções 
 
     
 class Contato():
@@ -25,8 +42,16 @@ class Contato():
         self.nome=nome
         self.telefone=telefone
         self.email=email
+    
+    def informacoes(self):
+        print("nome:"+self.nome+"\n telefone:"+ self.telefone+"\n email:"+self.email)
         
+    #atributos
     #nome 
     #email
     #telefone
     
+    #metodos
+
+contato1=Contato("rodrigo","987645327","rogrigo@ufpa.com")
+contato1.informacoes()
